e409c2b6e6d43956eea8460752dfcd939537b37f
Nailya <nmolutova@gmail.com>
2023-08-22 21:47:25 +0600

    Link

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,23 @@
 import React from "react";
 import ProductItem from "./ProductItem";
 import ProductCountryItem from "./ProductCounrtyItem";
+import { Link } from "react-router-dom";
 export default function Products({ dataToRender }) {
   return (
     <>
       {dataToRender.map((Coffee) => {
         if (Coffee.country) {
           return (
-            <ProductCountryItem
-              title={Coffee.name}
-              price={Coffee.price}
-              url={Coffee.url}
-              id={Coffee.id}
-              description={Coffee.description}
-              country={Coffee.country}
-            ></ProductCountryItem>
+            <Link to={`/ourcoffee/${Coffee.id}`}>
+              <ProductCountryItem
+                title={Coffee.name}
+                price={Coffee.price}
+                url={Coffee.url}
+                id={Coffee.id}
+                description={Coffee.description}
+                country={Coffee.country}
+              ></ProductCountryItem>
+            </Link>
           );
         } else {
           return (
