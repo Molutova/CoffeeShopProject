c93587fa89bf45cac953874aa1619451385a7a6d
Nailya <nmolutova@gmail.com>
2023-08-31 21:40:47 +0600

    one Our Coffee edit

--- src/pages/OneOurCoffee.js
+++ src/pages/OneOurCoffee.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import DataBase from "../db.json";
 
@@ -11,16 +11,38 @@ export default function OneOurCoffee() {
       return element;
     }
   });
+
+  const [oneCoffeeData, setOneCoffeeData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchOneCoffee = () => {
+      fetch(`http://localhost:4000/coffee/${paramsId}`)
+        .then((response) => response.json())
+        .then((result) => {
+          setOneCoffeeData({ ...result });
+        })
+
+        .catch((error) => console.log("error", error));
+    };
+    fetchOneCoffee();
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <h1>... Loading</h1>;
+  } else {
+    return (
+      <div>
+        <img src={oneCoffeeData.url}></img>
+        <h3> Product:{oneCoffeeData.name}</h3>
+        <p>Country: {oneCoffeeData.country}</p>
+        <p>Description:{oneCoffeeData.description}</p>
+        <p>Price:{oneCoffeeData.price}</p>
+      </div>
+    );
+  }
+
   // console.log(needToRender, "Its my Need To Render Object");
   // console.log("its my use params", useParams());
-
-  return (
-    <div>
-      <img src={needToRender.url}></img>
-      <h3> Product:{needToRender.name}</h3>
-      <p>Country: {needToRender.country}</p>
-      <p>Description:{needToRender.description}</p>
-      <p>Price:{needToRender.price}</p>
-    </div>
-  );
 }
